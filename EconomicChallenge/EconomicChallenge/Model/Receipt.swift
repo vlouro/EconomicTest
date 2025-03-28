ddbfeac817ccd023dad62d37ffdecffc1f39c1d1
moongyinterprojects <valterlouro@gmail.com>
2025-03-28 13:53:13 +0000

    Commit 3 - Added Receipt Model, Added Receipts entity
    Added Receipt Model
    Added Receipts entity to CoreData

---
+++ EconomicChallenge/EconomicChallenge/Model/Receipt.swift
@@ -0,0 +1,17 @@
+//
+//  Receipt.swift
+//  EconomicChallenge
+//
+//  Created by Valter Louro on 28/03/2025.
+//
+
+import UIKit
+
+struct Receipt {
+    let id: UUID
+    let name: String
+    let date: Date
+    let amount: Double
+    let currency: String
+    let image: UIImage
+}
