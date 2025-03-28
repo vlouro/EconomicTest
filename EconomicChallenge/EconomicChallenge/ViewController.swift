005671c26dacfe7ddec7b9ec36a4966aaaa3fff6
moongyinterprojects <valterlouro@gmail.com>
2025-03-28 13:32:45 +0000

    Commit 1 - Created Project
    Created XCode Project
    Set minimum version to iOS16

---
+++ EconomicChallenge/EconomicChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EconomicChallenge
+//
+//  Created by Valter Louro on 28/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EconomicChallenge/EconomicChallengeTests/EconomicChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  EconomicChallengeTests.swift
+//  EconomicChallengeTests
+//
+//  Created by Valter Louro on 28/03/2025.
+//
+
+import Testing
+@testable import EconomicChallenge
+
+struct EconomicChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
